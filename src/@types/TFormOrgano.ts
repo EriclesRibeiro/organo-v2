ea882f0415efb6495692e8cd81dca1cc78eba465
EriclesRibeiro <ericles.ribeiro@cronapp.io>
2024-02-15 19:16:08 -0300

    types de form separados

---
+++ src/@types/TFormOrgano.ts
@@ -0,0 +1,19 @@
+import { ImageListType } from "react-images-uploading"
+
+export type TFormOrgano = {
+    titulo: string,
+    subtitulo: string,
+    criador: string,
+    email: string,
+    descricao: string,
+    imagem: ImageListType | []
+}
+
+export type TFormCard = {
+    titulo: string,
+    subtitulo: string,
+    criador: string,
+    email: string,
+    descricao: string,
+    imagem: ImageListType | []
+}
