731c1ce69d09866ad798d4ec6b51095e4123afad
EriclesRibeiro <ericles.ribeiro@cronapp.io>
2024-02-18 17:38:19 -0300

    import do input de social-media

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,5 +1,6 @@
 import Button from "./button"
 import Input from "./input"
 import Label from "./label"
+import InputSocialMedia from "./input-social-media"
 
-export { Button, Input, Label }
+export { Button, Input, Label, InputSocialMedia }
